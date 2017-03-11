255c742785a29446dcd58f472f9ddc4a36369db4
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-03-11 09:30:08 +0100

    my first commit

---
+++ app/fizzBuzz.js
@@ -0,0 +1,27 @@
+﻿
+exports.fizzBuzz = function(number) {
+
+    var value_three = false;
+    var value_five = false;
+
+    if (number % 3 === 0) {
+        value_three = true;
+    }
+
+    if (number % 5 === 0) {
+        value_five = true;
+    }
+
+    if (value_three && value_five) {
+        return "FizzBuzz";
+    }
+    else if (value_three) {
+        return "Fizz";
+    }
+    else if (value_five) {
+        return "Buzz";
+    }
+    else {
+        return number;
+    }       
+}
